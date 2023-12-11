23ef638b418e7d569488407a40ad06308d8db8b4
Elliott Balsley <3991046+llamafilm@users.noreply.github.com>
2023-12-11 13:15:45 -0800

    fix version number

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "Syslog Sender",
-  Version = "0.0",
-  BuildVersion = "1.0.0.0",
+  Version = "1.0",
+  BuildVersion = "1.0.0.1",
   Id = "54c245c6-96d3-11ee-b9d1-0242ac120002",
   Author = "Elliott Balsley",
   Description = "Sends Event Log to a remote syslog server",
